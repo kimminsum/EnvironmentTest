74bad9890bf848150541350e09219468defa45da
kimminsum <tommy1005a@gmail.com>
2023-02-06 13:07:15 +0900

    Show Cells

--- main.py
+++ main.py
@@ -1,58 +1,86 @@
 """
 Title : Nature Environment Test
-L Energy System : if energy is 0 -> dead
+L Common System
+  Energy System : if energy is 0 -> dead
+  Reproduction Standard : Over Specific Energy -> Reproduction
 
 L Herbivores : Energy rises over time
-  Breeding : Energy Rise Rate, Size
+  Reproduction : Energy Rise Rate, Size
 
 L Carnivores : Predation on herbivores, Predation when carnivores double in size
-  Breeding : Speed, Detection range
+  Reproduction : Speed, Detection range
 """
 import pygame
 
-"""
 
-"""
 class Carnivores(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, radius, colour):
         super().__init__()
-    
-    def upadate(self):
-        pass
+        self.radius = radius
+        self.colour = colour
+        self.x, self.y = 10, 10
+        self.image = pygame.Surface((self.radius * 2, self.radius * 2), pygame.SRCALPHA)
+        pygame.draw.circle(self.image, self.colour, (self.radius, self.radius), self.radius)
+        self.rect = self.image.get_rect(center = (self.x, self.y))
+
+    def update(self):
+        self.image = pygame.Surface((self.radius *2, self.radius *2), pygame.SRCALPHA)
+        pygame.draw.circle(self.image, self.colour, (self.radius, self.radius), self.radius)
 
 
 class Predator(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, radius, colour):
         super().__init__()
+        self.radius = radius
+        self.colour = colour
+        self.x, self.y = 10, 50
+        self.image = pygame.Surface((self.radius * 2, self.radius * 2), pygame.SRCALPHA)
+        pygame.draw.circle(self.image, self.colour, (self.radius, self.radius), self.radius)
+        self.rect = self.image.get_rect(center = (self.x, self.y))
 
-    def upadate(self):
-        pass
+    def update(self):
+        self.image = pygame.Surface((self.radius *2, self.radius *2), pygame.SRCALPHA)
+        pygame.draw.circle(self.image, self.colour, (self.radius, self.radius), self.radius)
 
-
-class EnvironmentTest:
+class Ground:
     def __init__(self):
+        self.add_group()
+
+
+    def add_group(self):
+        self.carnivores_group = pygame.sprite.Group()
+        self.predator_group = pygame.sprite.Group()
+        self.common_group = pygame.sprite.Group()
+
+        for i in range(1):
+            carnivores = Carnivores(10, [255, 0, 0])
+            self.carnivores_group.add(carnivores)
+            self.common_group.add(carnivores)
+
+        for j in range(1):
+            predator = Predator(20, [0, 255, 0])
+            self.predator_group.add(predator)
+            self.common_group.add(predator)
+
+    def main(self):
         pygame.init()
+        pygame.display.set_caption("Environment Test")
+        screen = pygame.display.set_mode([500, 500]) # Set up the screen
 
-        # Set up the drawing window
-        screen = pygame.display.set_mode([500, 500])
-
-        # Run until the user asks to quit
-        running = True
+        running : bool = True
         while running:
-
-            # Did the user click the window close button?
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+                if event.type == pygame.QUIT: # If press X button -> Quit
                     running = False
 
-            # Fill the background with white
-            screen.fill((255, 255, 255))
+            screen.fill((255, 255, 255)) # Fill the background with white
+            self.common_group.draw(screen)
+            self.common_group.update()
+            pygame.display.update() # Update the screen
 
-            # Draw a solid blue circle in the center
-            pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+        pygame.quit() # Over the program
 
-            # Flip the display
-            pygame.display.flip()
 
-        # Done! Time to quit.
-        pygame.quit()
+if __name__=="__main__":
+    ground = Ground()
+    ground.main()
